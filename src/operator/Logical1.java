6ecd96f025dcfe8de005510e8f987ceea1dceb67
subins98 <0525year@naver.com>
2025-10-06 14:47:57 +0900

    연산자

--- src/operator/Logical1.java
+++ src/operator/Logical1.java
@@ -1,5 +1,27 @@
 package operator;
 
 public class Logical1 {
-    
+    static void main(String[] args) {
+        System.out.println("&&: AND 연산"); //모두가 참이어야 참
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && false);
+
+        System.out.println("||: OR 연산"); //하나라도 참이라면 참
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || false);
+
+        System.out.println("! 연산");
+        System.out.println(!true);
+        System.out.println(!false);
+
+        System.out.println("변수활용");
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+        System.out.println(!b);
+    }
 }
